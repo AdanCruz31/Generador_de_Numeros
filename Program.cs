0de01ee327e28b53839ce7ee85627635b92f0fcb
AdanCruz31 <a22021248@iteshu.edu.mx>
2024-09-24 19:53:31 -0600

    Proyecto de Simulacion 24 de Septiembre del 2024

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generador_de_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
